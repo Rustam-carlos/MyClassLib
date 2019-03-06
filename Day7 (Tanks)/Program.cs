93d3573279b3c44ee047600e53c73616365051e0
Rustam Galeev <Vincent-cat@mail.ru>
2019-03-06 17:48:53 +0600

    Добавьте файлы проекта.

---
+++ Day7 (Tanks)/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MyClassLib;
+using MyClassLib.WordOfTanks;
+
+namespace Day7__Tanks_
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 5;
+            Random rand = new Random();
+            Tank[] T34 = new Tank[size];
+            Tank[] pantera = new Tank[5];
+            Tank[] panthers = new Tank[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                panthers[i] = new Tank();
+            }
+            for (int i = 0; i < size; i++)
+            {
+                T34[i] = new Tank("T34", rand.Next(0, 100), rand.Next(0, 100), rand.Next(0, 100));
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                pantera[i] = new Tank("pantera", rand.Next(0, 100), rand.Next(0, 100), rand.Next(0, 100));
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                T34[i].Print();
+                pantera[i].Print();
+                panthers[i] = new Tank();
+                panthers[i] = T34[i] * pantera[i];
+                Console.WriteLine("+++++++++++++++++++++");
+                Console.WriteLine($"В {i + 1} бою победил");
+                panthers[i].Print();
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MyClassLib/WordOfTanks/Tank.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace MyClassLib.WordOfTanks
+{
+    public class Tank
+    {
+        private string type { get; set; }
+        private int ammunition { get; set; }
+        private int armor { get; set; }
+        private int mobility { get; set; }
+
+        public Tank()
+        {
+            type = "null";
+            ammunition = 0;
+            armor = 0;
+            mobility = 0;
+            
+        }
+        public Tank(string str ,int x, int y, int z)
+        {
+            type = str;
+            ammunition = x;
+            armor = y;
+            mobility = z;
+        }
+        
+        public void Print()
+        {
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("Танк \t\t\t\t- " + type);
+            Console.WriteLine("Колличество боеукладки\t\t - " + ammunition);
+            Console.WriteLine("Ссумарный коэфицент брони\t - " + armor);
+            Console.WriteLine("Ссумарный коэфицент маневренности - " + mobility);
+            Console.WriteLine("---------------------------------");
+        }
+
+        public static Tank operator *(Tank T34, Tank pantera)
+        {
+            if (T34.ammunition > pantera.ammunition && T34.armor > pantera.armor)
+            {
+                return T34;
+            }
+            if (T34.armor > pantera.armor && T34.mobility > pantera.mobility)
+            {
+                return T34;
+            }
+            if (T34.mobility > pantera.mobility && T34.ammunition > pantera.ammunition)
+            {
+                return T34;
+            }
+            else
+            {
+                return pantera;
+            }
+        }
+    }    
+}
